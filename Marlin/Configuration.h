eccb14428aa6ad50149a677dbae181f276ce171a
Miguel Almeida <migaxmoitax@gmail.com>
2018-02-23 17:19:21 +0000

    Reduce bed size, since it's actually 13.2 and not 14cm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -786,7 +786,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 120
-#define Y_BED_SIZE 140
+#define Y_BED_SIZE 130
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
